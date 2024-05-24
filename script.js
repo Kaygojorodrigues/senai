1080fc3c74554510547c29d01d2f3eb71b2b2a1e
Kaygojorodrigues <kaaayrodrigues0@gmail.com>
2024-05-24 14:33:22 -0500

    blaaaaaaaaaa

---
+++ script.js
@@ -0,0 +1,7 @@
+var nome = "AAAAAA"
+var idade = 6656577556
+var salariodobolsonaro = 4.528282852852
+
+document.write(nome + "<br>") 
+document.write(idade + "<br>")
+document.write(salariodobolsonaro)
