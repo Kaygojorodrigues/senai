ef01fc12f260e67f7d7e685961f19328403810cf
Kaygojorodrigues <kaaayrodrigues0@gmail.com>
2024-06-05 12:31:07 -0500

    aaaaaaaaaaaaaaaaaaaaaaa

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 var nome = "AAAAAA"
 var idade = 6656577556
 var salariodobolsonaro = 4.528282852852
+var resposta = prompt("qual é seu tipo de mulher? ")
 
 document.write(nome + "<br>") 
 document.write(idade + "<br>")
 document.write(salariodobolsonaro)
+console.log(nome)
+alert("meu tipo de mulher é: " + resposta)
+
---
+++ scriptaaaaaa.js
@@ -0,0 +1,9 @@
+var cadeia = "AAAAA"
+document.write(cadeia + "<br>")
+
+var number = 334354
+document.write(number)
+
+
+var mensagem = "BEM-VINDO!!!!!"
+alert(mensagem)
