6f948e255535e988982d7262315c7c2f95673ed8
Kaygojorodrigues <kaaayrodrigues0@gmail.com>
2024-07-03 14:59:42 -0500

    tgtyjftygfcfgcgc

--- java script/array.js
+++ java script/array.js
@@ -5,5 +5,13 @@ var gloss = ['carmed','maxlove','ruby rose','MELU','lua e neve','fenzza'];
 gloss.unshift('Gojo')
 gloss.shift('Gojo');
 gloss.push('Gojo');
+gloss.pop('Gojo');
+gloss.splice(2, 1)
 
 document.write(`<h1>${gloss}<h1>`);
+
+var achado = gloss.includes('carmed')
+document.write(`Você está atrás do Carmed? aqui está! ${achado}`)
+document.write(gloss.indexOf('maxlove'))
+gloss[2] = "Gojo"
+alert (gloss)
---
+++ java script/atividade_7.js
@@ -0,0 +1,16 @@
+var crush = ['gojo','kuroo','hiro hamada','megumi','cat noir'];
+
+
+document.write(crush[4] + "<br>");
+
+crush.push('Sunset Shimmer');
+
+document.write(crush + "<br>")
+
+document.write(crush[4] + "<br>")
+document.write(crush[3] + "<br>")
+
+crush.splice(2, 1)
+document.write(crush)
+
+
