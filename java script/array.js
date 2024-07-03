eaf4e189b2a5ba96483791dd3ff3df93a6522c5b
Kaygojorodrigues <kaaayrodrigues0@gmail.com>
2024-07-03 10:15:35 -0500

    gojo

---
+++ java script/array.js
@@ -0,0 +1,9 @@
+//var estados = ['Acre', 'São Paulo', 'Rio De Janeiro','Tóquio']
+//document.write(estados[3])
+
+var gloss = ['carmed','maxlove','ruby rose','MELU','lua e neve','fenzza'];
+gloss.unshift('Gojo')
+gloss.shift('Gojo');
+gloss.push('Gojo');
+
+document.write(`<h1>${gloss}<h1>`);
