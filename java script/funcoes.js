321664f68117d3f54e55dcb6cdb1f058641a7cfa
Kaygojorodrigues <kaaayrodrigues0@gmail.com>
2024-07-05 13:41:08 -0500

    AI BOLSONARO

---
+++ java script/funcoes.js
@@ -0,0 +1,43 @@
+
+     //function somar( num1, num2){
+        //return num1 + num2
+     //}
+    //var n1 = Number (prompt('digite um número: '))
+    //var n2 = Number(prompt('digite outro número: '))
+    //document.write(somar(n1, n2))
+
+    //var ana = ('Ana')
+    //function chamar(ana){
+        //return(ana)
+    //}
+    //document.write(ana)
+
+
+
+
+    //AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+
+   /* function somar( num1, num2, n3){
+       return num1 * num2 / n3
+     }
+    var n1 = Number (prompt('digite um número: '))
+    var n2 = Number (25)
+    var n3 = 100
+    document.write(somar(n1, n2, n3))*/
+
+
+    var par = (prompt('digite um número: ') % 2 ==0)
+    var num = 2
+    function par (num){
+    return num
+     }
+     if(par / num ){
+    alert('esse número é par')
+    }
+    else{
+        alert('esse número é ímpar')
+    }
+   
+   
+      
+
---
+++ java script/ta_repreendido.js
@@ -0,0 +1,9 @@
+ var toji = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+ var fushiguro = []
+     toji.forEach(function(valor){
+   if(valor < 15){
+    fushiguro.push(valor)
+   }
+  });
+  document.write(fushiguro)
+ 
