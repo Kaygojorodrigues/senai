ed60ad4165602db74a8f420cb444a4874a316b85
Kaygojorodrigues <kaaayrodrigues0@gmail.com>
2024-08-22 13:56:03 -0500

    banco de dados

---
+++ SQL/comandos.sql
@@ -0,0 +1,37 @@
+/*Comandos para criar database (bando de dados)*/
+
+CREATE DATABASE senai;
+
+
+/*Comandos para criar tabela*/
+
+CREATE TABLE Alunos(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCSHAR(255) NOT NULL,
+    email VARCSHAR(255)
+)
+
+/*Alterando o campo de uma tabela*/
+
+ALTER TABLE alunos
+CHANGE nome nome_pessoal VARCHAR(255)
+
+/*Mudar o nome de uma tabela*/
+RENAME TABLE aluno TO alunos
+
+/*comando para apagar o campo de uma tabela*/
+ALTER TABLE alunos DROP COLUMN nome;
+
+/*adicionar um campo na tabela criada*/
+
+ALTER TABLE alunos
+ADD descrição VARCHAR(255);
+
+/*adicionar itens nos campos*/
+INSERT INTO produtos
+
+/*caso você não colocou tudo junto e precise editar, use o UPDATE*/
+
+UPDATE produtos
+SET formula = 'hidratante', cor 'roxo'
+WHERE id = 1;
