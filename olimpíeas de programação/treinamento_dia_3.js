8e08547b8547a695b222487733aa20375d2f1027
Kaygojorodrigues <kaaayrodrigues0@gmail.com>
2024-09-27 16:26:28 -0500

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

---
+++ olimpíeas de programação/treinamento_dia_3.js
@@ -0,0 +1,24 @@
+//síncrona
+// var nome = "Ana"
+
+// document.write(nome)
+
+// var idade = 17
+
+// document.write(idade)
+
+// function nome(){
+//     return "Ana";
+// }
+
+// document.write(nome());
+
+//asíncrona
+
+var comida = "banana"
+console.log('teste1')
+setTimeout(function(){
+    console.log('teste3')
+}, 10000);
+
+console.log('teste2')
